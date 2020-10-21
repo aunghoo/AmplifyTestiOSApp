e0c2d5f630501219e53eca5b9a5c679fcde49785
aunghoo <aung@umich.edu>
2020-10-21 11:36:45 -0400

    Initial Commit

---
+++ Amplify-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Amplify-test
+//
+//  Created by Robert Aung on 10/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
